93ca8eefe028138592e01ce21b379b129838e080
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-04 11:27:41 +0700

    membuat kode untuk menampilkan notifiksi

--- app/src/main/java/com/gunder/simplenotif/MainActivity.kt
+++ app/src/main/java/com/gunder/simplenotif/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.gunder.simplenotif
 
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val NOTIFICATION_ID = 1
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    //    listener button
+    fun sendNotif(view: View) {
+        val mNotificarionManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notifications)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_notifications))
+            .setContentTitle(resources.getString(R.string.content_title))
+            .setContentText(resources.getString(R.string.content_text))
+            .setSubText(resources.getString(R.string.subtext))
+            .setAutoCancel(true)
+
+        val notification = mBuilder.build()
+        mNotificarionManager.notify(NOTIFICATION_ID, notification)
     }
 }
